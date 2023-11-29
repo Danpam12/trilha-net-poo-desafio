e4ea14e580fd8c3205a5318510407d1f5a3f4256
Danpam12 <dpbm@discente.ifpe.edu.br>
2023-11-29 09:22:09 -0300

    Desafio de projeto - Criando um  Sistema e Abstraindo um Celular com POO em C# Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Nokia myNokia = new Nokia(numero: "983999999", modelo: "Nokia G21", imei: "IMEI999", memoria: 128);
+myNokia.Ligar(); 
+myNokia.InstalarAplicativo("WhatsApp"); 
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone myIphone = new Iphone(numero: "986888888", modelo: "iPhone 15 Pro Max", imei: "IMEI777", memoria: 64);
+myIphone.ReceberLigacao();
+myIphone.InstalarAplicativo("Telegram");
+
